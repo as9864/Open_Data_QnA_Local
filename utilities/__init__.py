01df138707a21b83bb7861436d12ef4aefc82c30
SeungYeobHan <hanseungyeob9864@gmail.com>
2025-08-27 11:59:27 +0900

    chore: add local configuration example

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -5,9 +5,10 @@ import yaml
 
 config = configparser.ConfigParser()
 
+
 def is_root_dir():
     """
-    Checks if the current working directory is the root directory of a project 
+    Checks if the current working directory is the root directory of a project
     by looking for either the "/notebooks" or "/agents" folders.
 
     Returns:
@@ -18,12 +19,14 @@ def is_root_dir():
     print("current dir: ", current_dir)
     notebooks_path = os.path.join(current_dir, "notebooks")
     agents_path = os.path.join(current_dir, "agents")
-    
+
     return os.path.exists(notebooks_path) or os.path.exists(agents_path)
+
 
 def load_yaml(file_path: str) -> dict:
     with open(file_path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
+
 
 if is_root_dir():
     current_dir = os.getcwd()
@@ -31,12 +34,13 @@ if is_root_dir():
     root_dir = current_dir
 else:
     root_dir = os.path.abspath(os.path.join(os.getcwd(), '..'))
-    config.read(root_dir+'/config.ini')
+    config.read(root_dir + '/config.ini')
 
-if not 'root_dir' in locals():  # If not found in any parent dir
+if 'root_dir' not in locals():  # If not found in any parent dir
     raise FileNotFoundError("config.ini not found in current or parent directories.")
 
 print(f'root_dir set to: {root_dir}')
+
 
 def format_prompt(context_prompt, **kwargs):
     """
@@ -49,64 +53,65 @@ def format_prompt(context_prompt, **kwargs):
     """
     return context_prompt.format(**kwargs)
 
-# [CONFIG]
-EMBEDDING_MODEL = config['CONFIG']['EMBEDDING_MODEL']
-DESCRIPTION_MODEL = config['CONFIG']['DESCRIPTION_MODEL']
-# DATA_SOURCE = config['CONFIG']['DATA_SOURCE'] 
-VECTOR_STORE = config['CONFIG']['VECTOR_STORE']
 
-#CACHING = config.getboolean('CONFIG','CACHING')
-#DEBUGGING = config.getboolean('CONFIG','DEBUGGING')
-LOGGING = config.getboolean('CONFIG','LOGGING')
+# [CONFIG]
+MODE = config['CONFIG'].get('MODE', 'gcp').lower()
+
+LOGGING = config.getboolean('CONFIG', 'LOGGING')
 EXAMPLES = config.getboolean('CONFIG', 'KGQ_EXAMPLES')
 USE_SESSION_HISTORY = config.getboolean('CONFIG', 'USE_SESSION_HISTORY')
-USE_COLUMN_SAMPLES = config.getboolean('CONFIG','USE_COLUMN_SAMPLES')
+USE_COLUMN_SAMPLES = config.getboolean('CONFIG', 'USE_COLUMN_SAMPLES')
+FIRESTORE_REGION = config['CONFIG'].get('FIRESTORE_REGION', '')
 
-#[GCP]
-PROJECT_ID =  config['GCP']['PROJECT_ID']
+VECTOR_STORE = config['CONFIG'].get('VECTOR_STORE')
+EMBEDDING_MODEL = None
+DESCRIPTION_MODEL = None
 
-#[PGCLOUDSQL]
-PG_REGION = config['PGCLOUDSQL']['PG_REGION']
-# PG_SCHEMA = config['PGCLOUDSQL']['PG_SCHEMA'] 
-PG_INSTANCE = config['PGCLOUDSQL']['PG_INSTANCE']
-PG_DATABASE = config['PGCLOUDSQL']['PG_DATABASE'] 
-PG_USER = config['PGCLOUDSQL']['PG_USER'] 
-PG_PASSWORD = config['PGCLOUDSQL']['PG_PASSWORD']
+PROJECT_ID = PG_REGION = PG_INSTANCE = PG_DATABASE = PG_USER = PG_PASSWORD = None
+BQ_REGION = BQ_OPENDATAQNA_DATASET_NAME = BQ_LOG_TABLE_NAME = None
+PG_CONN_STRING = None
 
-#[BIGQUERY]
-BQ_REGION = config['BIGQUERY']['BQ_DATASET_REGION']
-# BQ_DATASET_NAME = config['BIGQUERY']['BQ_DATASET_NAME']
-BQ_OPENDATAQNA_DATASET_NAME = config['BIGQUERY']['BQ_OPENDATAQNA_DATASET_NAME']
-BQ_LOG_TABLE_NAME = config['BIGQUERY']['BQ_LOG_TABLE_NAME']
-# BQ_TABLE_LIST = config['BIGQUERY']['BQ_TABLE_LIST']
+if MODE == 'gcp':
+    EMBEDDING_MODEL = config['CONFIG']['EMBEDDING_MODEL']
+    DESCRIPTION_MODEL = config['CONFIG']['DESCRIPTION_MODEL']
+    PROJECT_ID = config['GCP']['PROJECT_ID']
+    PG_REGION = config['PGCLOUDSQL']['PG_REGION']
+    PG_INSTANCE = config['PGCLOUDSQL']['PG_INSTANCE']
+    PG_DATABASE = config['PGCLOUDSQL']['PG_DATABASE']
+    PG_USER = config['PGCLOUDSQL']['PG_USER']
+    PG_PASSWORD = config['PGCLOUDSQL']['PG_PASSWORD']
+    BQ_REGION = config['BIGQUERY']['BQ_DATASET_REGION']
+    BQ_OPENDATAQNA_DATASET_NAME = config['BIGQUERY']['BQ_OPENDATAQNA_DATASET_NAME']
+    BQ_LOG_TABLE_NAME = config['BIGQUERY']['BQ_LOG_TABLE_NAME']
+elif MODE == 'local':
+    PG_CONN_STRING = config['LOCAL']['PG_CONN_STRING']
+    EMBEDDING_MODEL = config['LOCAL']['EMBEDDING_MODEL_PATH']
+    DESCRIPTION_MODEL = config['LOCAL']['LLM_ENDPOINT']
 
-#[FIRESTORE]
-FIRESTORE_REGION = config['CONFIG']['FIRESTORE_REGION']
-
-#[PROMPTS]
+# [PROMPTS]
 PROMPTS = load_yaml(root_dir + '/prompts.yaml')
 
-__all__ = ["EMBEDDING_MODEL",
-           "DESCRIPTION_MODEL",
-          #"DATA_SOURCE",
-           "VECTOR_STORE",
-           #"CACHING",
-           #"DEBUGGING",
-           "LOGGING",
-           "EXAMPLES", 
-           "PROJECT_ID",
-           "PG_REGION",
-        #    "PG_SCHEMA",
-           "PG_INSTANCE",
-           "PG_DATABASE",
-           "PG_USER",
-           "PG_PASSWORD", 
-           "BQ_REGION",
-        #    "BQ_DATASET_NAME",
-           "BQ_OPENDATAQNA_DATASET_NAME",
-           "BQ_LOG_TABLE_NAME",
-        #    "BQ_TABLE_LIST",
-           "FIRESTORE_REGION",
-           "PROMPTS"
-           "root_dir",
-           "save_config"]
+__all__ = [
+    "MODE",
+    "EMBEDDING_MODEL",
+    "DESCRIPTION_MODEL",
+    "VECTOR_STORE",
+    "LOGGING",
+    "EXAMPLES",
+    "USE_SESSION_HISTORY",
+    "USE_COLUMN_SAMPLES",
+    "PROJECT_ID",
+    "PG_REGION",
+    "PG_INSTANCE",
+    "PG_DATABASE",
+    "PG_USER",
+    "PG_PASSWORD",
+    "PG_CONN_STRING",
+    "BQ_REGION",
+    "BQ_OPENDATAQNA_DATASET_NAME",
+    "BQ_LOG_TABLE_NAME",
+    "FIRESTORE_REGION",
+    "PROMPTS",
+    "root_dir",
+    "format_prompt",
+]
